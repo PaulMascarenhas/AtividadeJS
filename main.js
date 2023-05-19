df75672c06f21626a2e2a9e6efa9c90018a4436e
PaulMascarenhas <ppaulovitor565@gmail.com>
2023-05-19 08:45:58 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+function bah(){
+    let a = 5;
+    let b = 4;
+    let c = a+b;
+    console.log(c);
+    document.getElementById("apar").innerHTML = c;
+
+
+}
+
+function names(){
+    let d = "Luiz Arthur";
+    console.log(d);
+    document.getElementById("apar").innerHTML = d;
+
+}
